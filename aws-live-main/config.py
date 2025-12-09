6f593725c8b0c28037a43520516093b25b6efc73
hanzzz0607 <wonsh-wp22@student.tarc.edu.my>
2025-12-09 15:25:46 +0800

    Add files via upload

---
+++ aws-live-main/config.py
@@ -0,0 +1,7 @@
+customhost = "employee.cpk2igom42r0.us-east-1.rds.amazonaws.com"
+customuser = "aws_WonShiHan"
+custompass = "Bait3273"
+customdb = "employee"
+custombucket = "wonshihan-employee"
+customregion = "us-east-1"
+
