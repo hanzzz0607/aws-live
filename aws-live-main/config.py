edf42e9a7f5cabc6ed5c452b50b39c2edcac64a7
hanzzz0607 <wonsh-wp22@student.tarc.edu.my>
2025-12-09 15:46:28 +0800

    Delete aws-live-main directory

--- aws-live-main/config.py
+++
@@ -1,7 +0,0 @@
-customhost = "employee.cpk2igom42r0.us-east-1.rds.amazonaws.com"
-customuser = "aws_WonShiHan"
-custompass = "Bait3273"
-customdb = "employee"
-custombucket = "wonshihan-employee"
-customregion = "us-east-1"
-
